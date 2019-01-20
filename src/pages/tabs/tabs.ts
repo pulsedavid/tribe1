4512f6b9d2a97421340a7ef29637eb92b524798f
pulsedavid <dave.ontaptech@gmail.com>
2019-01-20 01:02:43 -0700

    Update tabs.ts

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {ViewChild} from '@angular/core';
-
 import { MyprofilePage } from '../myprofile/myprofile';
 import { ContactlistPage } from '../contactlist/contactlist';
 import { HomePage } from '../home/home';
@@ -10,18 +8,12 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
-@ViewChild('myTabs') tabRef: myTabs;
-
-ionViewDidEnter() {
-  this.tabRef.select(1);
- }
-
   tab1Root = MyprofilePage;
   tab2Root = HomePage;
   tab3Root = ContactlistPage;
 
   constructor() {
-
+    
   }
 
 }
